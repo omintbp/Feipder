37549eb08c2e257e8fa9b32d799fd0c6cbe4b19d
omintbp <zhiltcov.ds@gmail.com>
2023-04-28 18:30:39 +1000

    feat: add test endpoints for products

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Feipder.Models.ResponseModels;
+using Feipder.Tools;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Feipder.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    public class ProductController
+    public class ProductController : ControllerBase
     {
+        private readonly DataContext _dbContext;
 
+        public ProductController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// GET: api/products
+        /// </summary>
+        /// <returns>all products</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetProducts()
+        {
+            if(_dbContext.Products == null)
+            {
+                return NotFound();
+            }
+
+            var products = _dbContext.Products.Select((p) => new ProductResponse(p));
+
+            return new JsonResult(products);
+        }
     }
 }
---
+++ Models/ResponseModels/ProductResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Feipder.Models.ResponseModels
+{
+    public class ProductResponse
+    {
+        public ProductResponse(Product product)
+        {
+            Id = product.Id;
+            Article = product.Article;
+            Alias = product.Alias;
+            Description = product.Description;
+            Price = product.Price;
+        }
+
+        public int Id { get; set; }
+        public string Article { get; set; }
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
