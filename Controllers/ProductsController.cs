08b5bb47f2a17f9e55d49522de31588741c35a99
omintbp <zhiltcov.ds@gmail.com>
2023-04-30 18:46:05 +1000

    feat: add Get("category/{id}") endpoint

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Feipder.Models.ResponseModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.Versioning;
 
 namespace Feipder.Controllers
 {
@@ -34,6 +35,45 @@ namespace Feipder.Controllers
                 .Select((p) => new ProductResponse(p));
 
             return Ok(products);
+        }
+
+        [HttpGet("category/{id}")]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetProductsByCategory(int id)
+        {
+            if (!_dbContext.Products.Any() || !_dbContext.Categories.Any())
+            {
+                return NotFound();
+            }
+
+            var category = await _dbContext.Categories.FindAsync(id);
+                
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _dbContext.Products
+                .Include(x => x.Category)
+                .Where(x => x.Category.Id == id)
+                .Include(x => x.Brand)
+                .Select(x => 
+                    new {
+                        Id = x.Id,
+                        Article = x.Article,
+                        Description = x.Description,
+                        Price = x.Price,
+                        PreviewImage = x.PreviewImage,
+                        Alias = x.Alias,
+                        Brand = x.Brand,
+                        IsVisible = x.IsVIsible
+                    }
+                ).ToListAsync();
+
+            return Ok(new {
+                Id = category.Id,
+                Category = category.Name,
+                Products = products
+            });
         }
     }
 }
